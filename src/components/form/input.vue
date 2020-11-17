1961f9e3e56d8c71af7661b6b03a6b7f7868310f
ldc84 <ldc84@naver.com>
2020-11-17 23:47:39 +0900

    first commit

---
+++ src/components/form/input.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="input-box">
+    <input :type="type" :placeholder="placeholder" :class="className" />
+  </div>  
+</template>
+
+<script>
+export default {
+  props: {
+    type: String,
+    placeholder: String,
+    className: String,
+    size: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
---
+++ src/views/Join.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="join">
+    <h2>회원가입</h2>
+    <form class="join-form" action="">
+      <Input type="text" placeholder="아이디" v-model="id" />
+      <Input type="password" placeholder="비밀번호" v-model="password" />
+    </form>
+  </div>
+</template>
+<script>
+import Input from '@/components/form/input';
+
+export default {
+  components: {
+    Input,
+  },
+  data(){
+    return{
+      id: '',
+      password: '',
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+  #join {
+    h2 {
+
+    }
+    .join-form {
+      
+    }
+  }
+</style>
