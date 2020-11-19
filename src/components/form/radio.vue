2c31f1316d6df2af2840fc349063c87df09ae01c
ldc84 <ldc84@naver.com>
2020-11-19 19:02:28 +0900

    홈워크 진행중

---
+++ src/components/form/radio.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="radio-type">
+    <input 
+      type="radio" 
+      :value="label" 
+      :id="id" 
+      :name="name" 
+      v-model="radioButtonValue"
+    />
+    <label :for="id">
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    id: String,
+    label: String,
+    value: String,
+  },
+  computed: {
+    radioButtonValue: {
+      get: function() {
+        return this.value
+      },
+      set: function() {
+        this.$emit("change", this.label)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .radio-type {
+    position:relative;
+    input {
+      position:absolute;
+      top:0;
+      left:0;
+      width:0;
+      height:0;
+      opacity:0;
+    }
+    label {
+      display:block;
+      padding: 8px 15px;
+      color:#333;
+      font-size:14px;
+      border:none;
+      background-color:#f5f5f5;
+      box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 
+        0 2px 2px 0 rgba(0,0,0,.14), 
+        0 1px 5px 0 rgba(0,0,0,.12);
+      cursor: pointer;
+      text-align:center;
+    }
+    input:checked + label {
+      color:#fff;
+      background-color:#666;
+    }
+  }
+</style>
--- src/views/Join.vue
+++ src/views/Join.vue
@@ -2,28 +2,42 @@
   <div id="join">
     <h2>회원가입</h2>
     <form class="join-form" action="">
-      <Input type="text" placeholder="아이디" v-model="id" ref="id" />
-      <Input type="password" placeholder="비밀번호" v-model="password" />
+      <div class="form-control-box">
+        <Input type="text" placeholder="아이디" v-model="id" />
+      </div>
+      <div class="form-control-box">
+        <Input type="password" placeholder="비밀번호" v-model="password" />
+      </div>
+      <div class="form-control-box radio-flex">
+        <Radio name="sex" id="sexMale" label="남자" v-model="selectedSex" @change="onChangeSex" />
+        <Radio name="sex" id="sexfemale" label="여자" v-model="selectedSex" @change="onChangeSex" />
+      </div>
       <Button text="가입하기" className="floating" @click.native="onRequest" />
     </form>
   </div>
 </template>
 <script>
 import Input from '~@/form/input';
+import Radio from '~@/form/radio';
 import Button from '~@/form/button';
 
 export default {
   components: {
     Input,
-    Button
+    Button,
+    Radio
   },
   data(){
     return{
       id: '',
       password: '',
+      selectedSex: '남자'
     }
   },
   methods: {
+    onChangeSex(newValue){
+      this.selectedSex = newValue;
+    },
     onRequest(){
       // validation check
       if(this.id === ''){
@@ -46,7 +60,17 @@ export default {
 
     }
     .join-form {
-      
+      .form-control-box {
+        margin-top:10px;
+        &.radio-flex {
+          display:flex;
+          width:100%;
+          margin-top:20px;
+          > div {
+            width:50%;
+          }
+        }
+      }
     }
   }
 </style>
